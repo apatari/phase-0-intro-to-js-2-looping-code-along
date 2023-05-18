fb53f5bc469e8a2e5dd2e5b8c8bd13ce4ad4af54
Andy Patari <andy.patari@gmail.com>
2023-05-18 14:44:31 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+      debugger;
+    }
+  
+    return gifts;
+  }
+
+function writeCards(names, event) {
+    const messages = [];
+    for (let i =0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+
+function countDown(n) {
+  let counter = n;
+  while (counter >= 0) {
+    console.log(counter--);
+
+  }
+}
+
